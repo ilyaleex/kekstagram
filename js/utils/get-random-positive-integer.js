622e181350359277b2e6f90bd4c64f2ca38fa133
ilyaleex <gooooodguy@mail.ru>
2021-06-20 20:05:45 +0300

    resolving mistakes

--- js/utils/get-random-positive-integer.js
+++ js/utils/get-random-positive-integer.js
@@ -27,4 +27,4 @@ function getRandomPositiveInteger (a, b) {
   // И в конце с помощью метода Math.floor мы округляем полученный результат,
   // потому что Math.random() генерирует только дробные числа и ноль.
   return Math.floor(result);
-};
+}
